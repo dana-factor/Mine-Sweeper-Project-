a822e2b56178acaa5789f35d5e7ae8a7ec7df4f6
dana-factor <65674267+dana-factor@users.noreply.github.com>
2020-05-24 08:46:15 +0300

    Add files via upload

--- js/util.js
+++ js/util.js
@@ -5,3 +5,7 @@ function renderCell(i,j, value) {
     var elCell = document.querySelector(`#cell${i}-${j}`);
     elCell.innerText = value;
   };
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
